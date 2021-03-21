6b4fc97c183c4d3b9ef580c0bd4bb1014f3d31d7
Cristhian Munoz <cristhian.x.munoz@gmail.com>
2021-03-21 19:47:34 +0000

    adding material-ui to the dependencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel 
+      control = { <Checkbox 
+          checked ={checked}
+          onChange = {(e) => setChecked(e.target.checked)} /> 
+      } 
+      label = 'Testing stuff'
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TextField variant = 'outlined' type = 'date'/>
+        <CheckboxExample />
+        <ButtonGroup size="large" variant='contained' >
+          <Button 
+                endIcon = {<SaveIcon />}
+                color='secondary'>
+            Grupo Elba
+          </Button>
+          <Button 
+                endIcon = {<SaveIcon />}
+                color='primary'>
+            Grupo Elba
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
